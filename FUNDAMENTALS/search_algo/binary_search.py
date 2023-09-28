72be9c5809dcb8a856fc431ffd6a470afb5d3cdc
David <deivid.mitov@gmail.com>
2023-09-28 23:29:24 +0300

    fixed bugs

--- FUNDAMENTALS/search_algo/binary_search.py
+++ FUNDAMENTALS/search_algo/binary_search.py
@@ -1,17 +1,18 @@
 def binary_search(inp_list, target):
     start = 0
-    end = len(inp_list)
+    end = len(inp_list) - 1
 
-    while True:
+    while start <= end:
         search_index = (end + start) // 2
         if inp_list[search_index] < target:
             start = search_index + 1
         elif inp_list[search_index] > target:
-            end = search_index
+            end = search_index - 1
         elif inp_list[search_index] == target:
             return search_index
-        else:
-            return "Target not found"
+    else:
+        return "Target not found"
+
 
 #should be sorted list
 my_list = list(int(x) for x in input().split(", "))
