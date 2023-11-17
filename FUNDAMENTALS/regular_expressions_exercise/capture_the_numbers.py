17d0a4d2d810ef103e9caf0941126401774a1ea3
David <deivid.mitov@gmail.com>
2023-11-17 22:00:21 +0200

    regex exercise

--- FUNDAMENTALS/regular_expressions_exercise/capture_the_numbers.py
+++ FUNDAMENTALS/regular_expressions_exercise/capture_the_numbers.py
@@ -1,12 +1,12 @@
-    import re
+import re
 
-    pattern = r"\d+"
+pattern = r"\d+"
 
-    try:
-        while True:
-            line = input()
-            matches = re.finditer(pattern, line)
-            for match in matches:
-                print(match.group(), end=" ")
-    except EOFError:
-        pass  # Catch the EOFError when there is no more input
+try:
+    while True:
+        line = input()
+        matches = re.finditer(pattern, line)
+        for match in matches:
+            print(match.group(), end=" ")
+except EOFError:
+    pass  # Catch the EOFError when there is no more input
