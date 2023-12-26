939289dd54ef8075e218d901e7f3633fa875aca6
David Mitov <deivid.mitov@gmail.com>
2023-12-26 17:45:53 +0200

    recursion practice

---
+++ advanced/exercises/functions_advanced_exercise/recursion_palindrome.py
@@ -0,0 +1,10 @@
+def palindrome(word, idx):
+    if word[idx] != word[-(idx+1)]:
+        return f"{word} is not a palindrome"
+    if idx == len(word) // 2:
+        return f"{word} is a palindrome"
+
+    return palindrome(word, idx+1)
+
+
+print(palindrome("abcba", 0))
