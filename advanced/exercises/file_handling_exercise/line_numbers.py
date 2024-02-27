bbc731e0249b229cd9bce4dfed7a29143042f5b6
David Mitov <deivid.mitov@gmail.com>
2024-02-27 14:41:16 +0200

    exercise

---
+++ advanced/exercises/file_handling_exercise/line_numbers.py
@@ -0,0 +1,17 @@
+symbols = ["-", ",", ".", "!", "?", "'"]
+
+output_file = open("output_2.txt", "w")
+
+with open("text.txt", "r") as text:
+    lines = text.readlines()
+    index = 0
+    for line in lines:
+        index += 1
+        line = line.rstrip()
+        line_symbols = 0
+        for symbol in symbols:
+            if symbol in line:
+                line_symbols += line.count(symbol)
+        output_file.write(f"Line {index}: {line} ({len([x for x in line if x.isalpha()])})({line_symbols})\n")
+
+output_file.close()
