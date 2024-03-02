f8f33299542074e4bda29f088c9c9dc8c7754fdf
David Mitov <deivid.mitov@gmail.com>
2024-03-02 23:42:59 +0200

    exercise

---
+++ js_front_end/exercises/arrays_and_strings_exercise/print_every_nth.js
@@ -0,0 +1,15 @@
+function solve(array, number) {
+    result = []
+    for (let i = 0; i < array.length; i+= number) {
+        result.push(array[i])
+    }
+    return result
+}
+
+solve (['5', 
+'20', 
+'31', 
+'4', 
+'20'], 
+2
+)
