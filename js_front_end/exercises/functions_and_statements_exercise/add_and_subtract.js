398a5509d814e6d9eb50401edab81d7ae9780277
David Mitov <deivid.mitov@gmail.com>
2024-03-13 15:18:40 +0200

    exercise

---
+++ js_front_end/exercises/functions_and_statements_exercise/add_and_subtract.js
@@ -0,0 +1,11 @@
+function solve(a, b, c) {
+    const sum = (a, b) => a + b
+    const subtract = (a, b) => a - b
+
+    const result = subtract(sum(a, b), c)
+    console.log(result);
+}
+
+solve(23, 
+    6, 
+    10)
