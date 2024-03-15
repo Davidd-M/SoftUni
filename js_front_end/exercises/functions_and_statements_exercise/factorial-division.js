a1e5af97806731764dd4b3c2fc0c3537d5e30db7
David Mitov <deivid.mitov@gmail.com>
2024-03-15 23:05:17 +0200

    exercise

---
+++ js_front_end/exercises/functions_and_statements_exercise/factorial-division.js
@@ -0,0 +1,12 @@
+function solve(num1, num2) {
+    function calculateFactorial(num) {
+        if (num <= 1) {
+            return 1;
+        }
+        return num * calculateFactorial(num - 1);
+    }
+    let result = calculateFactorial(num1) / calculateFactorial(num2);
+    console.log(result.toFixed(2));
+}
+
+solve(5, 2);
