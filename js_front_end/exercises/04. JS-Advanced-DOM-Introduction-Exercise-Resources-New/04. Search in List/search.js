5fb82c23bdbc2b9d7b57fd33408d9d13ee0911f8
David Mitov <deivid.mitov@gmail.com>
2024-04-07 19:13:37 +0300

    exercise

--- js_front_end/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ js_front_end/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,20 @@
 function search() {
-   // TODO
+    const townsElement = document.getElementById("towns");
+    const resultElement = document.getElementById("result");
+    let searchedText = document.getElementById("searchText").value;
+    
+    let townsList = Array.from(townsElement.children)
+    let matches = 0
+
+    for (let town of townsList) {
+            town.style.textDecoration = "none";
+            town.style.fontWeight = "normal";
+        if (town.textContent.toLowerCase().includes(searchedText.toLowerCase())) {
+            town.style.textDecoration = "underline";
+            town.style.fontWeight = "bold";
+            matches++
+        }
+    }
+    
+    resultElement.textContent = `${matches} matches found`
 }
