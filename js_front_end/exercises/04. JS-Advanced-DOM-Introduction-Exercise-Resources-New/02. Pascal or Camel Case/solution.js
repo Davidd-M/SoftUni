be4d525f00a5d04a4ec638751d7527c268c71664
David Mitov <deivid.mitov@gmail.com>
2024-04-07 17:59:19 +0300

    exercise

--- js_front_end/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ js_front_end/exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-  //TODO...
+    const textElement = document.getElementById('text');
+    const namingConventionElement = document.getElementById('naming-convention');
+    const resultElement = document.querySelector('#result');
+
+    const text = textElement.value;
+    const namingConvention = namingConventionElement.value;
+
+    let converter = {
+        "Pascal Case": text => text.toLowerCase()
+            .split(" ")
+            .map(word => word[0].toUpperCase()
+                + word.slice(1)).join(""),
+
+        "Camel Case": text => text.toLowerCase()
+            .split(" ")
+            .map((word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1))
+            .join("")
+    }
+
+    let result = ""
+    if (!(Object.keys(converter).includes(namingConvention))) {
+        result = "Error!"
+    } else {
+        result = converter[namingConvention](text)
+    }
+
+    resultElement.textContent += result
+
 }
