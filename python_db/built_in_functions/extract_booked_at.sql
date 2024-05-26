689afba9471db03e3ede9c7cbf8343d88a8765ff
David <deivid.mitov@gmail.com>
2024-05-26 14:45:41 +0300

    exercise

---
+++ python_db/built_in_functions/extract_booked_at.sql
@@ -0,0 +1,9 @@
+SELECT
+	EXTRACT(YEAR FROM booked_at) as "YEAR",
+	EXTRACT(MONTH FROM booked_at) as "MONTH",
+	EXTRACT(DAY FROM booked_at) as "DAY",
+	EXTRACT(HOUR FROM booked_at AT TIME ZONE 'UTC') as "HOUR",
+	EXTRACT(MINUTE FROM booked_at) as "MINUTE",
+	CEILING(EXTRACT(SECOND FROM booked_at)) as "SECOND"
+FROM
+	bookings
