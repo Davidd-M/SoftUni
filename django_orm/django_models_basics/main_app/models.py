4063d755110b6cd21dde8da047058b44e8f93eac
David <deivid.mitov@gmail.com>
2024-06-19 01:16:14 +0300

    exercise

--- django_orm/django_models_basics/main_app/models.py
+++ django_orm/django_models_basics/main_app/models.py
@@ -118,3 +118,45 @@ class Exercise(models.Model):
         default=False,
     )
 
+
+class Book(models.Model):
+    GENRE_CHOICES = (
+        ('Fiction', 'Fiction'),
+        ('Non-Fiction', 'Non-Fiction'),
+        ('Science Fiction', 'Science Fiction'),
+        ('Horror', 'Horror'),
+    )
+
+    title = models.CharField(
+        max_length=30,
+    )
+
+    author = models.CharField(
+        max_length=100,
+    )
+
+    genre = models.CharField(
+        max_length=20,
+        choices=GENRE_CHOICES,
+    )
+
+    publication_date = models.DateField(
+        editable=False,
+        auto_now_add=True,
+    )
+
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+    )
+
+    is_available = models.BooleanField(
+        default=True,
+    )
+
+    rating = models.FloatField()
+
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
