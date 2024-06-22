203ca6d41b544ac77a798d26a0175060133fe261
David <deivid.mitov@gmail.com>
2024-06-22 15:31:00 +0300

    exercise

--- django_orm/data_operations_and_queries/caller.py
+++ django_orm/data_operations_and_queries/caller.py
@@ -1,11 +1,12 @@
 import os
 import django
+from django.db.models import QuerySet
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import Pet, Artifact
+from main_app.models import Pet, Artifact, Location
 
 
 def create_pet(name: str, species: str):
@@ -42,9 +43,35 @@ def rename_artifact(artifact: Artifact, new_name: str):
 def delete_all_artifacts():
     Artifact.objects.all().delete()
 
-
 # print(create_artifact('Ancient Sword', 'Lost Kingdom', 500, 'A legendary sword with a rich history', True))
 # artifact_object = Artifact.objects.get(name='Ancient Sword')
 # rename_artifact(artifact_object, 'Ancient Shield')
 # print(artifact_object.name)
 
+
+def show_all_locations() -> str:
+    result = []
+
+    for location in Location.objects.all().order_by("-id"):
+        result.append(f"{location.name} has a population of {location.population}!")
+
+    return '\n'.join(result)
+
+
+def new_capital():
+    first_location = Location.objects.first()
+    if first_location is not None:
+        first_location.is_capital = True
+        first_location.save()
+
+
+def get_capitals() -> QuerySet:
+    return Location.objects.filter(is_capital=True).values("name")
+
+
+def delete_first_location():
+    Location.objects.first().delete()
+
+print(show_all_locations())
+print(new_capital())
+print(get_capitals())
