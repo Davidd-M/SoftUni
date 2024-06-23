513e5c51736c627acc38ebcadd8498158e9f0444
David <deivid.mitov@gmail.com>
2024-06-23 11:29:38 +0300

    exercise

---
+++ django_orm/working_with_queries/caller.py
@@ -0,0 +1,40 @@
+import os
+import django
+
+from populate_db import populate_model_with_data
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+
+from main_app.models import ArtworkGallery
+
+
+def show_highest_rated_art() -> str:
+    highest_rated = ArtworkGallery.objects.order_by("-rating", "id").first()
+
+    return f"{highest_rated.art_name} is the highest-rated art with a {highest_rated.rating} rating!"
+
+
+def bulk_create_arts(first_art: ArtworkGallery, second_art: ArtworkGallery) -> None:
+    ArtworkGallery.objects.bulk_create(
+        [
+            first_art,
+            second_art,
+         ],
+    )
+
+
+def delete_negative_rated_arts() -> None:
+    ArtworkGallery.objects.filter(rating__lt=0).delete()
+
+
+artwork1 = ArtworkGallery(artist_name='Vincent van Gogh', art_name='Starry Night', rating=4, price=1200000.0)
+artwork2 = ArtworkGallery(artist_name='Leonardo da Vinci', art_name='Mona Lisa', rating=5, price=1500000.0)
+
+# Bulk saves the instances
+bulk_create_arts(artwork1, artwork2)
+print(show_highest_rated_art())
+print(ArtworkGallery.objects.all())
+
