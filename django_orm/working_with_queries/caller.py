14bdeb1dc8583facc134c2652e85d9abd9082623
David <deivid.mitov@gmail.com>
2024-06-23 14:25:19 +0300

    exercise

--- django_orm/working_with_queries/caller.py
+++ django_orm/working_with_queries/caller.py
@@ -2,14 +2,14 @@ import os
 from typing import List
 
 import django
-
+from django.db.models import Case, When, Value, F
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 
-from main_app.models import ArtworkGallery, Laptop, ChessPlayer
+from main_app.models import ArtworkGallery, Laptop, ChessPlayer, Meal
 
 
 def show_highest_rated_art() -> str:
@@ -188,3 +188,65 @@ def grand_chess_title_regular_player() -> None:
 # print("Number of Chess Players after deletion:", ChessPlayer.objects.count())
 
 
+def set_new_chefs() -> None:
+    Meal.objects.filter(meal_type='Breakfast').update(chef='Gordon Ramsay')
+    Meal.objects.filter(meal_type='Lunch').update(chef='Julia Child')
+    Meal.objects.filter(meal_type='Dinner').update(chef='Jamie Oliver')
+    Meal.objects.filter(meal_type='Snack').update(chef='Thomas Keller')
+
+
+def set_new_preparation_times() -> None:
+    Meal.objects.update(
+        preparation_time=Case(
+            When(meal_type='Breakfast', then=Value('10 minutes')),
+            When(meal_type='Lunch', then=Value('12 minutes')),
+            When(meal_type='Dinner', then=Value('15 minutes')),
+            When(meal_type='Snack', then=Value('5 minutes')),
+        )
+    )
+
+
+def update_low_calorie_meals() -> None:
+    Meal.objects.filter(meal_type__in=('Breakfast', 'Dinner')).update(calories=400)
+
+
+def update_high_calorie_meals() -> None:
+    Meal.objects.filter(meal_type__in=('Lunch', 'Snack')).update(calories=700)
+
+
+def delete_lunch_and_snack_meals() -> None:
+    Meal.objects.filter(meal_type__in=('Lunch', 'Snack')).delete()
+
+
+# meal1 = Meal.objects.create(
+#     name="Pancakes",
+#     meal_type="Breakfast",
+#     preparation_time="20 minutes",
+#     difficulty=3,
+#     calories=350,
+#     chef="Jane",
+# )
+#
+# meal2 = Meal.objects.create(
+#     name="Spaghetti Bolognese",
+#     meal_type="Dinner",
+#     preparation_time="45 minutes",
+#     difficulty=4,
+#     calories=550,
+#     chef="Sarah",
+# )
+# # Test the set_new_chefs function
+# set_new_chefs()
+#
+# # Test the set_new_preparation_times function
+# set_new_preparation_times()
+#
+# # Refreshes the instances
+# meal1.refresh_from_db()
+# meal2.refresh_from_db()
+#
+# # Print the updated meal information
+# print("Meal 1 Chef:", meal1.chef)
+# print("Meal 1 Preparation Time:", meal1.preparation_time)
+# print("Meal 2 Chef:", meal2.chef)
+# print("Meal 2 Preparation Time:", meal2.preparation_time)
