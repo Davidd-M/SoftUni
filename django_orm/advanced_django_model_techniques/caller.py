f2508d2d3222d01e170d1ed6549fc90818683955
David <deivid.mitov@gmail.com>
2024-07-02 14:46:37 +0300

    exercise

---
+++ django_orm/advanced_django_model_techniques/caller.py
@@ -0,0 +1,25 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+from main_app.models import Customer
+from django.core.exceptions import ValidationError
+
+# customer = Customer(
+#     name="Svetlin Nakov1",
+#     age=1,
+#     email="nakov@example",
+#     phone_number="+35912345678",
+#     website_url="htsatps://nakov.com/"
+# )
+#
+# try:
+#     customer.full_clean()
+#     customer.save()
+#
+# except ValidationError as e:
+#     print('\n'.join(e.messages))
+#
